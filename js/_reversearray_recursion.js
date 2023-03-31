0e9bc8494c3b4557a656cc17a247fdadd62e996e
uttam pun <uttampun62@gmail.com>
2023-03-31 08:22:31 +0545

    recursion added

---
+++ js/_reversearray_recursion.js
@@ -0,0 +1,13 @@
+"use strict";
+let arr = [29, 0, 1, 56, 3, 43, 56, 23, 45];
+let temp;
+function reverseArray(arr, start, end) {
+    if (start <= end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start + 1, end - 1);
+        console.log(arr);
+    }
+}
+reverseArray(arr, 0, arr.length - 1);
